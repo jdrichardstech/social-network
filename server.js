b34f704fdb4310b3f2d6eaa311b62f5a5e6360c5
jdrichardstech <jdrichardstech@gmail.com>
2019-08-29 12:27:02 -0400

    add client

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const connectDB = require("./config/db");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,6 +11,7 @@ connectDB();
 
 // Initialize Middleware
 app.use(express.json({ extended: false }));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("API Running");
