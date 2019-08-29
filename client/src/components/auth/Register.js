784e3fede19ed36d6980f0c4f53df748e9834874
jdrichardstech <jdrichardstech@gmail.com>
2019-08-29 19:10:31 -0400

    connect register to redux with error checking

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -3,13 +3,14 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
     password: '',
-    password2: '',
+    password2: ''
   });
 
   const { name, email, password, password2 } = formData;
@@ -22,30 +23,7 @@ const Register = ({ setAlert }) => {
     if (password !== password2) {
       setAlert('Passwords do not match', 'danger');
     } else {
-      const newFormData = {
-        name,
-        email,
-        password,
-      };
-
-      try {
-        const config = {
-          header: {
-            'Content-Type': 'application/json',
-          },
-        };
-        // const body = JSON.stringify(newFormData);
-
-        const res = await axios.post(
-          'http://localhost:5000/api/users/',
-          newFormData,
-          config,
-        );
-        console.log(res.data);
-        return;
-      } catch (err) {
-        console.error(err);
-      }
+      register({ name, email, password });
     }
   };
 
@@ -63,7 +41,6 @@ const Register = ({ setAlert }) => {
             name="name"
             value={name}
             onChange={e => handleChange(e)}
-            required
           />
         </div>
         <div className="form-group">
@@ -109,5 +86,5 @@ const Register = ({ setAlert }) => {
 
 export default connect(
   null,
-  { setAlert },
+  { setAlert, register }
 )(Register);
